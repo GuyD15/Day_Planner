3e11a543f4d1838a90aedd36e6db608410e6036e
GuyD15 <guywayne.domino7@gmail.com>
2023-06-20 19:48:33 -0500

    Added comments for JS file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// attaches an event handler with all elements that have the saveBtn class. when clicked the id attribute which is the hour of time block is received. Also retrieves the information in the text area.
 $(function () {
     $('.saveBtn').on('click', function () {
       var hour = $(this).parent().attr('id');
@@ -6,10 +7,10 @@ $(function () {
       localStorage.setItem(hour, text);
     });
   });
-  
+
   $(document).ready(function () {
     $('.saveBtn').on('click', function () {});
-  
+//   updates each time block with the correct class depending on the time of day. The class that is assigned is working with the CSS file to determine what color the block will be. Also it retrieves the current hour by using dayjs().hour(). 
     function hourUpdater() {
         var currentHour = dayjs().hour();
         $('.time-block').each(function () {
@@ -27,7 +28,7 @@ $(function () {
   
     hourUpdater();
     setInterval(hourUpdater, 15000);
-
+// sets the loop to iterate of ther hours of 9am to 5pm
     for (var i = 9; i <= 17; i++) {
         $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i));
     }
